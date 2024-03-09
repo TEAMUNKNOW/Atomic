93583a615bf2aafdc796e81c8891210db5613f1a
TEAMUNKNOW <142662898+TEAMUNKNOW@users.noreply.github.com>
2024-03-09 23:08:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(getenv("API_ID", "20837006"))
 API_HASH = getenv("API_HASH", "b7c5f5ea03b19b1687cddf9eceaf69aa")
 BOT_TOKEN = getenv("BOT_TOKEN", "5669078885:AAFV796FLMIWGVbw0uhV9RVZa9IpscaB_kg")
 OWNER_ID = int(getenv("OWNER_ID", "5673255098"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5673255098").split()))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://abhiroy989186:9relnjjmd3neu1Nt@cluster0.ad2kusl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 SESSION_STRING = getenv("SESSION_STRING", "BQE1F74AMiZ83oyhUFv0P049qhNetHG-5eN5lf1hYlWLukwvnjUA93uEIFIKoVq7vdo82UvWlOW8giwGGUxGXTqK6yFh0i4WXlaK0UlVIeCYYy9dshbbEAxyUc4as5WvpIdLMBH8uBpjxkKgAJ6ZGVsp3R4lEQiQzwnhxR8GCHYZCODoqUyOi2BrtCGB09jpKKkhCpqC3bu0Rs7E1-q8sQoMzrz23fa2wkHNxfgMBxlZx-y7w3JWmxU7K3tfHNLkjDn3FthZGR-kYpRAxoASKIg9A0ELTLBp7_vfwd5wdVUaIrdx7_uMBC-Gq9lBvMH7ua4T6rKTaQ-UbK2hqZmZnFAC3ZfeYgAAAAFcFcJLAA")
 
