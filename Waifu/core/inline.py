de168c79841b96b3d8c3b412903bc2b69877970b
TEAMUNKNOW <142662898+TEAMUNKNOW@users.noreply.github.com>
2024-03-10 09:27:10 +0530

    Update core

---
+++ Waifu/core/inline.py
@@ -0,0 +1,7 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+import config
+
+close_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="✯ ᴄʟᴏsᴇ ✯", callback_data="close")]]
+)
